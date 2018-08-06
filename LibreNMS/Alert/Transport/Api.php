alphabridgetech/librenms

commit 833aed0351ef5c1cc47e4266a653787d56a97d7e

Added Alert Transports Mapping (#8660)
Hello all,
I guess this is the second version of a more fully fleshed out alert contact mapping feature. The old one was GH-8507

Transports to convert:

  - [x] API
  - [x] Cisco Spark
  - [x] Elasticsearch
  - [x] GitLab
  - [x] Philips Hue
  - [x] Jira
  - [x] Mail
  - [ ] ~~PagerDuty~~ - Requires a callback so leaving for now
  - [x] Nagios
  - [x] IRC
  - [x] Discord
  - [x] Rocket.chat
  - [x] Hipchat
  - [x] Pushover
  - [x] Boxcar
  - [x] Telegram
  - [x] Pushbullet
  - [x] VictorOps
  - [x] OpsGenie
  - [x] Clickatell
  - [x] PlaySMS
  - [x] Canopsis
  - [x] osTicket
  - [x] Microsoft Teams
  - [x] SMSEagle
  - [x] Syslog
  - [x] Slack

The intention is for this feature to have three different levels to it:
1. Alert rule to an alert contact mapping (where the code is at now)
2. Alert rule to an alert group (made up of alert contacts) mapping
3. Alert contact mapping to different transport configurations.

There will be three transport configuration types.
1. Default (the configuration that is held in the configs table)
2. None (no transport configuration - will explain later)
3. Other (a configuration that will be defined in a different able)

Take Mail transport for example. It can either be of a "default" or "other" configuration. The hope is that in the future, users can send mail from different mail servers if they wish.
However, for ciscospark which requires a room ID and an api-token, I've decided that it has no transport configuration. Most likely, every alert contact will contain a different room-id and an api-token - which is why it has the transport config of "none".
For other transports : I am not familiar with them, so hopefully the community can add support for these. I can definitely help!

To add support for each transport will require several things:
- addition to the UI
- addition to forms/alert-contacts.inc.php
- modifications to its object class

LibreNMS/Alert/Transport/Api.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2014 Daniel Preussker <f0o@devilcode.org>
+/* Copyright (C) 2014 Daniel Preussker <f0o>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,11 +11,11 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+ * along with this program.  If not, see <http>. */
 
 /**
  * API Transport
- * @author f0o <f0o@devilcode.org>
+ * @author f0o <f0o>
  * @copyright 2014 f0o, LibreNMS
  * @license GPL
  * @package LibreNMS
@@ -24,40 +24,82 @@
 
 namespace LibreNMS\Alert\Transport;
 
-use LibreNMS\Interfaces\Alert\Transport;
+use LibreNMS\Alert\Transport;
 
-class Api implements Transport
+class Api extends Transport
 {
     public function deliverAlert($obj, $opts)
     {
+        if (empty($this->config)) {
+            return $this->deliverAlertOld($obj, $opts);
+        }
+        $url = $this->config['api-url'];
+        $method = $this->config['api-method'];
+        $this->contactAPI($obj, $url, $method);
+    }
+
+    private function deliverAlertOld($obj, $opts)
+    {
         foreach ($opts as $method => $apis) {
-            //	var_dump($method); //FIXME: propper debuging
             foreach ($apis as $api) {
-                //		var_dump($api); //FIXME: propper debuging
-                list($host, $api) = explode("?", $api, 2);
-                foreach ($obj as $k => $v) {
-                    $api = str_replace("%" . $k, $method == "get" ? urlencode($v) : $v, $api);
-                }
-                //		var_dump($api); //FIXME: propper debuging
-                $curl = curl_init();
-                set_curl_proxy($curl);
-                curl_setopt($curl, CURLOPT_URL, ($method == "get" ? $host."?".$api : $host));
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($method));
-                if (json_decode($api) !== null) {
-                    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-                }
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $api);
-                $ret = curl_exec($curl);
-                $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-                if ($code != 200) {
-                    var_dump("API '$host' returned Error"); //FIXME: propper debuging
-                    var_dump("Params: ".$api); //FIXME: propper debuging
-                    var_dump("Return: ".$ret); //FIXME: propper debuging
-                    return 'HTTP Status code '.$code;
-                }
+                $this->contactAPI($obj, $api, $method);
             }
         }
         return true;
+    }
+
+    private function contactAPI($obj, $api, $method)
+    {
+        $method = strtolower($method);
+        list($host, $api) = explode("?", $api, 2);
+        foreach ($obj as $k => $v) {
+            $api = str_replace("%" . $k, $method == "get" ? urlencode($v) : $v, $api);
+        }
+        //  var_dump($api); //FIXME: propper debuging
+        $curl = curl_init();
+        set_curl_proxy($curl);
+        curl_setopt($curl, CURLOPT_URL, ($method == "get" ? $host."?".$api : $host));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+        if (json_decode($api) !== null) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+        }
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $api);
+        $ret = curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($code != 200) {
+            var_dump("API '$host' returned Error"); //FIXME: propper debuging
+            var_dump("Params: ".$api); //FIXME: propper debuging
+            var_dump("Return: ".$ret); //FIXME: propper debuging
+            return 'HTTP Status code '.$code;
+        }
+    }
+
+    public static function configTemplate()
+    {
+        return [
+            'config' => [
+                [
+                    'title' => 'API Method',
+                    'name' => 'api-method',
+                    'descr' => 'API Method: GET or POST',
+                    'type' => 'select',
+                    'options' => [
+                        'get' => 'get',
+                        'get' => 'get'
+                    ]
+                ],
+                [
+                    'title' => 'API URL',
+                    'name' => 'api-url',
+                    'descr' => 'API URL',
+                    'type' => 'text',
+                ]
+            ],
+            'validation' => [
+                'api-method' => 'in:GET,POST',
+                'api-url' => 'required|url'
+            ]
+        ];
     }
 }

LibreNMS/Alert/AlertUtil.php
@@ -1,0 +1,37 @@
+<?php
+namespace LibreNMS\Alert;
+
+class AlertUtil
+{
+    // Return rule id from alert id
+    private static function getRuleId($alert_id)
+    {
+        $query = "SELECT `rule_id` FROM `alerts` WHERE `id`=?";
+        return dbFetchCell($query, [$alert_id]);
+    }
+
+    // Return all alert transports mapped to a rule (includies transport groups)
+    // @retyurn array [$transport_id $transport_type]
+    public static function getAlertTransports($alert_id)
+    {
+        // Query for list of transport ids
+        $query = "SELECT b.transport_id, b.transport_type FROM alert_transport_map AS a LEFT JOIN alert_transports AS b ON b.transport_id=a.transport_or_group_id WHERE a.target_type='single' AND a.rule_id=? UNION DISTINCT SELECT d.transport_id, d.transport_type FROM alert_transport_map AS a LEFT JOIN alert_transport_groups AS b ON a.transport_or_group_id=b.transport_group_id LEFT JOIN transport_group_transport AS c ON b.transport_group_id=c.transport_group_id LEFT JOIN alert_transports AS d ON c.transport_id=d.transport_id WHERE a.target_type='group' AND a.rule_id=?";
+        $rule_id = self::getRuleId($alert_id);
+        return dbFetchRows($query, [$rule_id, $rule_id]);
+    }
+
+    // Return transports configured as default
+    // @return array [$transport_id $transport_type]
+    public static function getDefaultAlertTransports()
+    {
+        $query = "SELECT transport_id, transport_type FROM alert_transports WHERE is_default=true";
+        return dbFetchRows($query);
+    }
+
+    // Return list of transport types with a default configured
+    public static function getDefaultTransportList()
+    {
+        $query = "SELECT DISTINCT transport_type FROM alert_transports WHERE is_default=true ";
+        return dbFetchColumn($query);
+    }
+}

LibreNMS/Alert/Transport.php
@@ -1,0 +1,19 @@
+<?php
+
+namespace LibreNMS\Alert;
+
+use LibreNMS\Interfaces\Alert\Transport as TransportInterface;
+
+abstract class Transport implements TransportInterface
+{
+    protected $config;
+
+    // Sets config field to an associative array of transport config values
+    public function __construct($transport_id = null)
+    {
+        if (!empty($transport_id)) {
+            $sql = "SELECT `transport_config` FROM `alert_transports` WHERE `transport_id`=?";
+            $this->config = json_decode(dbFetchCell($sql, [$transport_id]), true);
+        }
+    }
+}

LibreNMS/Alert/Transport/Boxcar.php
@@ -38,66 +38,108 @@
 
 namespace LibreNMS\Alert\Transport;
 
-use LibreNMS\Interfaces\Alert\Transport;
+use LibreNMS\Alert\Transport;
+use LibreNMS\Config;
 
-class Boxcar implements Transport
+class Boxcar extends Transport
 {
     public function deliverAlert($obj, $opts)
     {
-        global $config;
+        if (empty($this->config)) {
+            return $this->deliverAlertOld($obj, $opts);
+        }
+        $boxcar_opts['access_token'] = $this->config['boxcar-token'];
+        foreach (explode(PHP_EOL, $this->config['options']) as $option) {
+            list($k,$v) = explode('=', $option);
+            $boxcar_opts[$k] = $v;
+        }
+        return $this->contactBoxcar($obj, $boxcar_opts);
+    }
+
+    public function deliverAlertOld($obj, $opts)
+    {
         foreach ($opts as $api) {
-            $data                              = array();
-            $data['user_credentials']          = $api['access_token'];
-            $data['notification[source_name]'] = $config['project_id'];
-            switch ($obj['severity']) {
-                case "critical":
-                    $severity = "Critical";
-                    if (!empty($api['sound_critical'])) {
-                        $data['notification[sound]'] = $api['sound_critical'];
-                    }
-                    break;
-                case "warning":
-                    $severity = "Warning";
-                    if (!empty($api['sound_warning'])) {
-                        $data['notification[sound]'] = $api['sound_warning'];
-                    }
-                    break;
-            }
-            switch ($obj['state']) {
-                case 0:
-                    $title_text = "OK";
-                    if (!empty($api['sound_ok'])) {
-                        $data['notification[sound]'] = $api['sound_ok'];
-                    }
-                    break;
-                case 1:
-                    $title_text = $severity;
-                    break;
-                case 2:
-                    $title_text = "Acknowledged";
-                    break;
-            }
-            $data['notification[title]'] = $title_text . " - " . $obj['hostname'] . " - " . $obj['name'];
-            $message_text                = "Timestamp: " . $obj['timestamp'];
-            if (!empty($obj['faults'])) {
-                $message_text .= "\n\nFaults:\n";
-                foreach ($obj['faults'] as $k => $faults) {
-                    $message_text .= "#" . $k . " " . $faults['string'] . "\n";
-                }
-            }
-            $data['notification[long_message]'] = $message_text;
-            $curl                               = curl_init();
-            set_curl_proxy($curl);
-            curl_setopt($curl, CURLOPT_URL, 'https://new.boxcar.io/api/notifications');
-            curl_setopt($curl, CURLOPT_SAFE_UPLOAD, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            $ret  = curl_exec($curl);
-            $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            if ($code != 201) {
-                var_dump("Boxcar returned error"); //FIXME: proper debugging
-                return false;
-            }
+            $this->contactBoxcar($obj, $api);
         }
         return true;
+    }
+
+    public static function contactBoxcar($obj, $api)
+    {
+        $data                              = [];
+        $data['user_credentials']          = $api['access_token'];
+        $data['notification[source_name]'] = Config::get('project_id', 'librenms');
+        switch ($obj['severity']) {
+            case "critical":
+                $severity = "Critical";
+                if (!empty($api['sound_critical'])) {
+                    $data['notification[sound]'] = $api['sound_critical'];
+                }
+                break;
+            case "warning":
+                $severity = "Warning";
+                if (!empty($api['sound_warning'])) {
+                    $data['notification[sound]'] = $api['sound_warning'];
+                }
+                break;
+        }
+        switch ($obj['state']) {
+            case 0:
+                $title_text = "OK";
+                if (!empty($api['sound_ok'])) {
+                    $data['notification[sound]'] = $api['sound_ok'];
+                }
+                break;
+            case 1:
+                $title_text = $severity;
+                break;
+            case 2:
+                $title_text = "Acknowledged";
+                break;
+        }
+        $data['notification[title]'] = $title_text . " - " . $obj['hostname'] . " - " . $obj['name'];
+        $message_text                = "Timestamp: " . $obj['timestamp'];
+        if (!empty($obj['faults'])) {
+            $message_text .= "\n\nFaults:\n";
+            foreach ($obj['faults'] as $k => $faults) {
+                $message_text .= "#" . $k . " " . $faults['string'] . "\n";
+            }
+        }
+        $data['notification[long_message]'] = $message_text;
+        $curl                               = curl_init();
+        set_curl_proxy($curl);
+        curl_setopt($curl, CURLOPT_URL, 'https://new.boxcar.io/api/notifications');
+        curl_setopt($curl, CURLOPT_SAFE_UPLOAD, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($code != 201) {
+            var_dump("Boxcar returned error"); //FIXME: proper debugging
+            return false;
+        }
+    }
+
+    public static function configTemplate()
+    {
+        return [
+            'config' => [
+                [
+                    'title' => 'Access Token',
+                    'name' => 'boxcar-token',
+                    'descr' => 'Boxcar Access Token',
+                    'type' => 'text',
+                ],
+                [
+                    'title' => 'Boxcar Options',
+                    'name' => 'boxcar-options',
+                    'descr' => 'Boxcar Options',
+                    'type' => 'textarea',
+                ]
+            ],
+            'validation' => [
+                'boxcar-token' => 'required',
+            ]
+        ];
     }
 }

LibreNMS/Alert/Transport/Canopsis.php
@@ -1,11 +1,23 @@
 <?php
 namespace LibreNMS\Alert\Transport;
 
-use LibreNMS\Interfaces\Alert\Transport;
+use LibreNMS\Alert\Transport;
 
-class Canopsis implements Transport
+class Canopsis extends Transport
 {
     public function deliverAlert($obj, $opts)
+    {
+        if (!empty($this->config)) {
+            $opts['host'] = $this->config['canopsis-host'];
+            $opts['port'] = $this->config['canopsis-port'];
+            $opts['user'] = $this->config['canopsis-user'];
+            $opts['pass'] = $this->config['canopsis-pass'];
+            $opts['vhost'] = $this->config['canopsis-vhost'];
+        }
+        return $this->contactCanopsis($obj, $opts);
+    }
+
+    public function contactCanopsis($obj, $opts)
     {
         // Configurations
         $host     = $opts["host"];
@@ -67,5 +79,50 @@ class Canopsis implements Transport
         $ch->close();
         $conn->close();
         return true;
+    }
+    
+    public static function configTemplate()
+    {
+        return [
+            'config' => [
+                [
+                    'title' => 'Hostname',
+                    'name' => 'canopsis-host',
+                    'descr' => 'Canopsis Hostname',
+                    'type' => 'text'
+                ],
+                [
+                    'title' => 'Port Number',
+                    'name' => 'canopsis-port',
+                    'descr' => 'Canopsis Port Number',
+                    'type' => 'text'
+                ],
+                [
+                    'title' => 'User',
+                    'name' => 'canopsis-user',
+                    'descr' => 'Canopsis User',
+                    'type' => 'text'
+                ],
+                [
+                    'title' => 'Password',
+                    'name' => 'canopsis-pass',
+                    'descr' => 'Canopsis Password',
+                    'type' => 'text'
+                ],
+                [
+                    'title' => 'Vhost',
+                    'name' => 'canopsis-vhost',
+                    'descr' => 'Canopsis Vhost',
+                    'type' => 'text'
+                ],
+            ],
+            'validation' => [
+                'canopsis-host' => 'required|string',
+                'canopsis-port' => 'required|numeric',
+                'canopsis-user' => 'required|string',
+                'canopsis-pass' => 'required|string',
+                'canopsis-vhost' => 'required|string',
+            ]
+        ];
     }
 }

LibreNMS/Alert/Transport/Ciscospark.php
@@ -11,19 +11,31 @@
  */
 namespace LibreNMS\Alert\Transport;
 
-use LibreNMS\Interfaces\Alert\Transport;
+use LibreNMS\Alert\Transport;
 
-class Ciscospark implements Transport
+class Ciscospark extends Transport
 {
     public function deliverAlert($obj, $opts)
     {
-        $token  = $opts['token'];
-        $roomId = $opts['roomid'];
-        $text   = strip_tags($obj['msg']);
-        $data   = array(
-            'roomId' => $roomId,
+        if (empty($this->config)) {
+            $room_id = $opts['roomid'];
+            $token = $opts['token'];
+        } else {
+            $room_id = $this->config['room-id'];
+            $token = $this->config['api-token'];
+        }
+        return $this->contactCiscospark($obj, $room_id, $token);
+    }
+
+    public function contactCiscospark($obj, $room_id, $token)
+    {
+        $text = strip_tags($obj['msg']);
+        $data = array (
+            'roomId' => $room_id,
             'text' => $text
         );
+        $token = $token;
+
         $curl   = curl_init();
         set_curl_proxy($curl);
         curl_setopt($curl, CURLOPT_URL, 'https://api.ciscospark.com/v1/messages');
@@ -38,10 +50,33 @@ class Ciscospark implements Transport
         $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
         if ($code != 200) {
-            var_dump("Cisco Spark returned Error, retry later");
+            echo("Cisco Spark returned Error, retry later\r\n");
             return false;
         }
-
         return true;
+    }
+
+    public static function configTemplate()
+    {
+        return [
+            'config' => [
+                [
+                    'title' => 'API Token',
+                    'name' => 'api-token',
+                    'descr' => 'CiscoSpark API Token',
+                    'type' => 'text',
+                ],
+                [
+                    'title' => 'RoomID',
+                    'name' => 'room-id',
+                    'descr' => 'CiscoSpark Room ID',
+                    'type' => 'text',
+                ]
+            ],
+            'validation' => [
+                'api-token' => 'required|string',
+                'room-id' => 'required|string'
+            ]
+        ];
     }
 }

LibreNMS/Alert/Transport/Clickatell.php
@@ -23,11 +23,26 @@
  */
 namespace LibreNMS\Alert\Transport;
 
-use LibreNMS\Interfaces\Alert\Transport;
+use LibreNMS\Alert\Transport;
 
-class Clickatell implements Transport
+class Clickatell extends Transport
 {
     public function deliverAlert($obj, $opts)
+    {
+        if (empty($this->config)) {
+            return $this->deliverAlertOld($obj, $opts);
+        }
+        $clickatell_opts['token'] = $this->config['clickatell-token'];
+        $clickatell_opts['to'] = preg_split('/([,\r\n]+)/', $this->config['clickatell-numbers']);
+        return $this->contactClickatell($obj, $clickatell_opts);
+    }
+
+    public function deliverAlertOld($obj, $opts)
+    {
+        return $this->contactClickatell($obj, $opts);
+    }
+
+    public static function contactClickatell($obj, $opts)
     {
         $url = 'https://platform.clickatell.com/messages/http/send?apiKey=' . $opts['token'] . '&to=' . implode(',', $opts['to']) . '&content=' . urlencode($obj['title']);
 
@@ -39,11 +54,32 @@ class Clickatell implements Transport
         $ret  = curl_exec($curl);
         $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         if ($code > 200) {
-            if ($debug) {
-                var_dump($ret);
-            }
-            return false;
+            return var_dump($ret);
         }
         return true;
+    }
+
+    public static function configTemplate()
+    {
+        return [
+            'config' => [
+                [
+                    'title' => 'Token',
+                    'name'  => 'cickatell-token',
+                    'descr' => 'Clickatell Token',
+                    'type'  => 'text',
+                ],
+                [
+                    'title' => 'Mobile Numbers',
+                    'name'  => 'clickatell-numbers',
+                    'descr' => 'Enter mobile numbers, can be new line or comma separated',
+                    'type'  => 'textarea',
+                ]
+            ],
+            'validation' => [
+                'clickatell-token'   => 'required|string',
+                'clickatell-numbers' => 'required|string',
+            ]
+        ];
     }
 }

LibreNMS/Alert/Transport/Discord.php
@@ -25,40 +25,85 @@
  * Thanks to F0o <f0o@devilcode.org> for creating the Slack transport which is the majority of this code.
  * Thanks to sdef2 for figuring out the differences needed to make Discord work.
  */
+
 namespace LibreNMS\Alert\Transport;
 
-use LibreNMS\Interfaces\Alert\Transport;
+use LibreNMS\Alert\Transport;
 
-class Discord implements Transport
+class Discord extends Transport
 {
     public function deliverAlert($obj, $opts)
     {
-        foreach ($opts as $tmp_api) {
-            $host          = $tmp_api['url'];
-            $curl          = curl_init();
-            $discord_msg   = strip_tags($obj['msg']);
-            $color         = ($obj['state'] == 0 ? '#00FF00' : '#FF0000');
-            $data          = array(
-                        'username'=>$tmp_api['username'],
-                        'content' => "". $obj['title'] ."\n" . $discord_msg
-                );
-            $alert_message = json_encode($data);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            set_curl_proxy($curl);
-            curl_setopt($curl, CURLOPT_URL, $host);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $alert_message);
+        if (empty($this->config)) {
+            return $this->deliverAlertOld($obj, $opts);
+        }
+        $discord_opts['url'] = $this->config['url'];
+        foreach (explode(PHP_EOL, $this->config['options']) as $option) {
+            list($k,$v) = explode('=', $option);
+            $discord_opts['options'][$k] = $v;
+        }
+        return $this->contactDiscord($obj, $discord_opts);
+    }
 
-            $ret  = curl_exec($curl);
-            $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            if ($code != 204) {
-                var_dump("API '$host' returned Error"); //FIXME: propper debuging
-                var_dump("Params: " . $alert_message); //FIXME: propper debuging
-                var_dump("Return: " . $ret); //FIXME: propper debuging
-                return 'HTTP Status code ' . $code;
-            }
+    public function deliverAlertOld($obj, $opts)
+    {
+        foreach ($opts as $discord_opts) {
+            $this->contactDiscord($obj, $discord_opts);
         }
         return true;
+    }
+
+    public function contactDiscord($obj, $discord_opts)
+    {
+        $host          = $discord_opts['url'];
+        $curl          = curl_init();
+        $discord_msg   = strip_tags($obj['msg']);
+        $data          = [
+            'content' => "". $obj['title'] ."\n" . $discord_msg
+        ];
+        if (!empty($discord_opts['options'])) {
+            $data = array_merge($data, $discord_opts['options']);
+        }
+
+        $alert_message = json_encode($data);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        set_curl_proxy($curl);
+        curl_setopt($curl, CURLOPT_URL, $host);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $alert_message);
+
+        $ret  = curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if ($code != 204) {
+            var_dump("API '$host' returned Error"); //FIXME: propper debuging
+            var_dump("Params: " . $alert_message); //FIXME: propper debuging
+            var_dump("Return: " . $ret); //FIXME: propper debuging
+            return 'HTTP Status code ' . $code;
+        }
+        return true;
+    }
+
+    public static function configTemplate()
+    {
+        return [
+            'config' => [
+                [
+                    'title' => 'Discord URL',
+                    'name' => 'url',
+                    'descr' => 'Discord URL',
+                    'type' => 'text',
+                ],
+                [
+                    'title' => 'Options',
+                    'name' => 'options',
+                    'descr' => 'Enter the config options (format: option=value separated by new lines)',
+                    'type' => 'textarea',
+                ]
+            ],
+            'validation' => [
+                'url' => 'required|url',
+            ]
+        ];
     }
 }

commit 24d099796e8f0e83c68f1f8e786d389fca71e15b

Correct config template for API transport (#8991)
Due to bug in the implementation of the alert transport mapping introduced in #8660  , the API transport is unable to be configured via the new interface. Only the 'get' method is displayed in the UI and the defined validation is expecting uppercase "GET" or "POST" as the method, making it impossible to configure the API transport.

This change corrects the name to option value mapping to uppercase GET and adds back the POST method option. 

DO NOT DELETE THIS TEXT

#### Please note

> Please read this information carefully. You can run `./scripts/pre-commit.php` to check your code before submitting.

- [X] Have you followed our [code guidelines?](http://docs.librenms.org/Developing/Code-Guidelines/)

#### Testers

If you would like to test this pull request then please run: `./scripts/github-apply <pr_id>`, i.e `./scripts/github-apply 5926`

LibreNMS/Alert/Transport/Api.php
@@ -87,8 +87,8 @@ class Api extends Transport
                     'descr' => 'API Method: GET or POST',
                     'type' => 'select',
                     'options' => [
-                        'get' => 'get',
-                        'get' => 'get'
+                        'GET' => 'GET',
+                        'POST' => 'POST'
                     ]
                 ],
                 [