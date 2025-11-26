c2be27013b8c8174b5e05cded1c1afe7e327f162
alphabridgetech <alphabridgetech@gmail.com>
2025-11-26 12:21:58 +0530

    configratation home page create

--- resources/views/device/tabs/config/home.blade.php
+++ resources/views/device/tabs/config/home.blade.php
@@ -1,1 +1,59 @@
-<h1>home</h1>
+
+<div class="container-fluid" style="margin-top: 20px;">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">System Information</h3>
+        </div>
+
+        <div class="panel-body">
+            <table class="table table-striped table-bordered" style="margin:0;">
+                <tbody>
+                    <tr>
+                        <th width="200">Device Type</th>
+                        <td>AS228T</td>
+                    </tr>
+                    <tr>
+                        <th>BIOS Version</th>
+                        <td>0.5.3</td>
+                    </tr>
+                    <tr>
+                        <th>Firmware Version</th>
+                        <td>2.2.0D Build 127313</td>
+                    </tr>
+                    <tr>
+                        <th>Serial Num</th>
+                        <td>G20008059033</td>
+                    </tr>
+                    <tr>
+                        <th>MAC Address</th>
+                        <td>8C:1F:64:20:EA:4E</td>
+                    </tr>
+                    <tr>
+                        <th>IP Address</th>
+                        <td>192.168.200.244</td>
+                    </tr>
+                    <tr>
+                        <th>Current Time</th>
+                        <td>2000-1-14 1:38:18</td>
+                    </tr>
+                    <tr>
+                        <th>Uptime</th>
+                        <td>13 day -1 hour -38 minute -18 second</td>
+                    </tr>
+                    <tr>
+                        <th>CPU Usage</th>
+                        <td>9%</td>
+                    </tr>
+                    <tr>
+                        <th>Memory Usage</th>
+                        <td>44%</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="panel-footer text-right">
+            <button class="btn btn-default">Refresh</button>
+        </div>
+    </div>
+</div>
