1449a43be87e2f73094f226c0ef86115e4857e93
alphabridgetech <alphabridgetech@gmail.com>
2025-11-27 14:42:00 +0530

    dynamic {device_ip}.yml add according device

--- resources/views/device/tabs/config/kiwi.blade.php
+++ resources/views/device/tabs/config/kiwi.blade.php
@@ -1,1 +1,5 @@
-<h1>kiwi</h1>
+<h4>Device Details</h4>
+
+<p><strong>Hostname:</strong> {{ $device->hostname }}</p>
+<p><strong>SSH User:</strong> {{ $device->ssh_user }}</p>
+<p><strong>SSH Password:</strong> {{ $device->ssh_pass }}</p>
