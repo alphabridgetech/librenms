d7d670c32a45572bbc4fc83c2f550abb11176220
alphabridgetech <alphabridgetech@gmail.com>
2026-01-03 15:45:03 +0530

    remove alphabridge old config

--- app/View/Components/Device/PageTabs.php
+++ app/View/Components/Device/PageTabs.php
@@ -71,7 +71,7 @@ class PageTabs extends Component
         'qos' => \App\Http\Controllers\Device\Tabs\QosController::class,
         'latency' => \App\Http\Controllers\Device\Tabs\LatencyController::class,
         'configuration' => \App\Http\Controllers\Device\Tabs\ConfigurationController::class,
-        'alphabridge' => \App\Http\Controllers\Device\Tabs\alphabridgeController::class,
+        //'alphabridge' => \App\Http\Controllers\Device\Tabs\alphabridgeController::class,
         'nac' => \App\Http\Controllers\Device\Tabs\NacController::class,
         'notes' => \App\Http\Controllers\Device\Tabs\NotesController::class,
         'edit' => \App\Http\Controllers\Device\Tabs\EditController::class,
